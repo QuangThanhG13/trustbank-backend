2e9ceccce17b9d76d4cc033165842d18f76c123f
Dung <dungtranngoc1412@gmail.com>
2024-12-26 21:02:38 +0700

    First Commit

---
+++ src/main/java/com/cloud_student/trustbank/TrustBankApplication.java
@@ -0,0 +1,13 @@
+package com.cloud_student.trustbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud_student/trustbank/TrustBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_student.trustbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
